b45d783be93455426e7e2fc4700e10ea83d24585
Cristóbal Andrés Cortés Cofré <cristobal.cortes@alu.ucm.cl>
2024-03-25 10:43:23 -0300

    comentarios

--- Speech_Recognition.py
+++ Speech_Recognition.py
@@ -8,29 +8,29 @@ import speech_recognition as sr
 r = sr.Recognizer()
 
 def grabacion_texto():
-    with sr.Microphone() as mic:
-        print("Háblale al micrófono...")
+    with sr.Microphone() as mic: 
+        print("Hablale al microfono...")
         r.adjust_for_ambient_noise(mic)
-        audio = r.listen(mic)
+        audio = r.listen(mic) #el sonido de mic que escucha lo guarda en audio
 
         try:
-            texto = r.recognize_google(audio, language='es-ES')
-            if texto == "140":
+            texto = r.recognize_google(audio, language='es-ES') #Se utiliza Google para reconocer el audio hacia texto
+            if texto == "140": #si se dice el numero 140 el programa se detiene
                 raise SystemExit
             return texto
-        except sr.UnknownValueError:
+        except sr.UnknownValueError: #En caso de no entender el audio el programa lanzara un error 
             print("No se pudo entender el audio.")
-        except sr.RequestError as e:
+        except sr.RequestError as e: #en caso de tener problema 
             print(f"Error en la solicitud: {e}")
 
-def guardar_texto(texto):
-    with open("Audio_a_Texto.txt", "a") as f:
+def guardar_texto(texto): #definicion que permite guardar el texto en un archivo .txt
+    with open("Audio_a_Texto.txt", "a") as f: 
         f.write(texto + "\n")
 
-while True:
+while True: #mientra se grabe, se procedera a pedir que el usuario hable, si no se escucha o entiende nada, se procedera a imprimir "Intentalo de nuevo"
     texto = grabacion_texto()
     if texto:
         print(f"Texto reconocido: {texto}")
         guardar_texto(texto)
     else:
-        print("Inténtalo de nuevo.")
+        print("Intentalo de nuevo.")
